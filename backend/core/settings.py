c698564137e885f0a56086fbd03e3d8258bc05aa
Isaac <luis.lopez@peaku.ai>
2022-11-21 21:53:52 -0500

    Backend modifies

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -61,18 +61,18 @@ MIDDLEWARE = [
 
 ]
 
-# CORS_ORIGIN_WHITELIST=(
-#     'http://localhost:8000'
-#     'http://127.0.0.1:8000'
+CORS_ORIGIN_WHITELIST=[
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
 
 
-#     'http://localhost:8080'
-#     'http://127.0.0.1:8080'
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
 
 
-#     'http://localhost:3000'
-#     'http://127.0.0.1:3000'
-# )
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
 
 CORS_ALLOW_HEADERS= list(default_headers) + [
     'contentype',
